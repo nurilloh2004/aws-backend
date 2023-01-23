3fb7946628d24936c6f5208e633e0ec8e2147c61
nurilloh2004 <nurillohpython@gmail.com>
2023-01-23 15:12:13 +0500

    ad

--- app/host/views.py
+++ app/host/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from core.models import Category
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
 from .serializers import CategorySerializer
 
@@ -10,3 +13,11 @@ class CategoryListView(generics.ListAPIView):
 
     def get_queryset(self):
         return Category.objects.filter(is_active=True)
+
+class CategoryView(APIView):
+    def post(self, request):
+        serializer = CategorySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
