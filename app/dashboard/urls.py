c6af429dad1b7599540e91fefb2782b4bcaee8a0
nurilloh_anv <88767460+nurilloh2004@users.noreply.github.com>
2023-02-01 10:41:33 +0500

    added

--- app/dashboard/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import CategoryListView, CategoryView
-urlpatterns = [
-    path('categories/', CategoryListView.as_view(), name='category_list'),
-    path('category/', CategoryView.as_view(), name='category'),
-]
