8ec7ebcddb4ea725513258d5841e5c03ac1b450b
nurilloh_anv <88767460+nurilloh2004@users.noreply.github.com>
2023-02-01 14:57:28 +0500

    added

--- confing/settings.py
+++ confing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k2g*7^#(*brc459waer$6aroqm(--+v6q=(t#_6nfs*w)b2k3&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
