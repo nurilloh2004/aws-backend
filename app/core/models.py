e4968281876a08e0613ea4d6b4d918ff4145a09f
nurilloh_anv <88767460+nurilloh2004@users.noreply.github.com>
2023-02-02 17:02:12 +0500

    ad

--- app/core/models.py
+++ app/core/models.py
@@ -4,6 +4,7 @@ Database models.
 import uuid
 import os
 
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
@@ -63,6 +64,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     def __str__(self) -> str:
         return self.name
 
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Userlar")
 
 class Category(models.Model):
     """Category model for navbar site."""
@@ -75,14 +79,113 @@ class Category(models.Model):
     def __str__(self):
         return self.title
 
+    class Meta:
+        verbose_name = _("Kategoriya")
+        verbose_name_plural = _("Kategoriyalar")
 
 
+class Banner(models.Model):
+    """For Banner space on web info."""
+    name = models.CharField(max_length=255)
+    title = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = _("Banner")
+        verbose_name_plural = _("Banners")
+
+class About(models.Model):
+    """For about page info."""
+    name = models.CharField(max_length=255)
+    title = models.TextField(blank=True, null=True)
+    image = models.ImageField(null=True, upload_to=recipe_image_file_path)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = _("Info")
+        verbose_name_plural = _("Info")
+    
+
+class Services(models.Model):
+    """Hosting services model."""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=255)
+    RAM = models.CharField(max_length=255)
+    SSD = models.CharField(max_length=255)
+    CPU = models.CharField(max_length=255)
+    extra_storage = models.CharField(max_length=255)
+    backup = models.BooleanField()
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=2, max_length=3)
+    price_year = models.DecimalField(max_digits=2, max_length=3, blank=True, null=True)
 
 
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name = _("Servis")
+        verbose_name_plural = _("Servislar")
 
 
+class ItemsDomain(models.Model):
+    """For dynamically data for langing page."""
+    name = models.CharField(max_length=255)
+    title = models.TextField(blank=True, null=True)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    class Meta:
+        verbose_name = _("Domen haqida")
+        verbose_name_plural = _("Domenlar haqida")
 
 
+class Domain(models.Model):
+    """For domain selling model."""
+    Product_Status = (
+        ('.uz', '.uz'),
+        ('.ru', '.ru'),
+        ('.com', '.com'),
+        ('.info', '.info'),
+    )
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=20, choices=Product_Status, default='.uz', null=True, blank=True)
+    price = models.DecimalField(max_digits=2, max_length=3)
+    price_year = models.DecimalField(max_digits=2, max_length=3, blank=True, null=True)
+
+
+    def __str__(self) -> str:
+        return self.name
+
+
+    class Meta:
+        verbose_name = _("Domen")
+        verbose_name_plural = _("Domenlar")
+
+
+class OrderDomain(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+    
+    class Meta:
+        verbose_name = _("Domen buyurtma")
+        verbose_name_plural = _("Domen buyurtmalari")
 
 
 
