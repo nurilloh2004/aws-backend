22919fd1b182a5ca9a48ffe96b799b86a179d381
nurilloh_anv <88767460+nurilloh2004@users.noreply.github.com>
2023-02-03 12:00:03 +0500

    ad

--- app/web/urls.py
+++ app/web/urls.py
@@ -5,7 +5,7 @@ from .views import *
 urlpatterns = [
     path('categories/', CategoryListCreateAPIView.as_view(), name='category-list-create'),
     path('categories/<int:pk>/', CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category-retrieve-update-destroy'),
-    path('banners/', BannerDetail.as_view(), name='banner-list'),
+    path('banners/<int:pk>/', BannerDetail.as_view(), name='banner-detail'),
     path('abouts/', AboutList.as_view(), name='about-list'),
     path('services/', ServicesList.as_view(), name='services-list'),
     path('services/<int:pk>/', ServicesDetail.as_view(), name='services-detail'),
